09c1c96617f7b24ee882762606b69b2188553729
unknown <abhishekkumar4434@gmail.com>
2018-06-09 22:14:03 -0400

    Updating

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
+const port = process.env.PORT || 3000;
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getcurrentYear',() => {
 return new Date().getFullYear();
@@ -43,6 +44,6 @@ app.get('/about',(req,res) => {
     
    });
 })
-app.listen(3000, () => {
-    console.log('Server is up and running');
+app.listen(port, () => {
+    console.log(`Server is up and running at ${port}`);
 });
